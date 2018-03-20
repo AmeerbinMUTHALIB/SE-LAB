35cfb970a434a88f8a401975a6cb7329f36cf407
saahilk <kamatsaahil@gmail.com>
2018-03-20 23:20:19 +0530

    login1.2.1

--- src/login.php
+++ src/login.php
@@ -7,11 +7,11 @@
 
 
 <?php
-// session_start();
-// if(isset($_SESSION["user_id"]) && $_SESSION["user_id"]!= "")
-// {
-// 	$_SESSION["user_id"]= "";
-// }
+session_start();
+if(isset($_SESSION["user_id"]) && $_SESSION["user_id"]!= "")
+{
+	$_SESSION["user_id"]= "";
+}
 ?>
 
 <style>
