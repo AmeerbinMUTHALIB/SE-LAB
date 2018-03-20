08084d0cea656dc9f0b3bf2b1d32f3453eb09e70
swaroop-kaza <kazasrisaiswaroop@gmail.com>
2018-03-20 20:01:16 +0530

    sample

---
+++ scr/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+  <head>
+    <title>NITC</title>
+    <link rel="icon" href="">
+    <link rel="stylesheet" type="text/css" href="">
+  </head>
+  <body>
+    <div>
+      <div class="header">
+        <a href="http://www.nitc.ac.in/"><img src=""></a>
+        <div class="div1">
+          <h2>NATIONAL INSTITUTE OF TECHNOLOGY CALICUT</h2>
+          <br>
+          <h2></h2>
+        </div>
+      </div>
+      <div class="div2">
+        <form action="action/action_index.php" method="POST">
+          <div>
+              STUDENT USER NAME:
+              <input type="text" name='USERNAME' placeholder="USER NAME" />
+              <br>
+              <br>
+              PASSWORD:
+              <input type="password" name='PASSWORD' placeholder="PASSWORD" />
+              <br>
+              <br>
+              <a href="" id="forgot_pw">Forgot Password</a>
+              <br>
+              <br>
+          </div>
+          <button>login</button>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
