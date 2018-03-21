9a9abe1310325825e3ff9316f974a53d716e57fb
swaroop-kaza <kazasrisaiswaroop@gmail.com>
2018-03-21 12:13:34 +0530

    hello

--- index.php
+++ index.php
@@ -71,9 +71,6 @@ span.psw {
 <h2>Login Form</h2>
 
 <form action="/action_page.php">
-  <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
 
   <div class="container">
     <label for="uname"><b>Username</b></label>
